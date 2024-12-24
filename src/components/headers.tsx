2520bc9e3e34f56c4c31ccb30c507b5f18c663d7
Nin Dang <nindang12345@gmail.com~>
2024-12-24 14:53:15 +0700

    feat:header

--- src/components/headers.tsx
+++ src/components/headers.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Image from "next/image";
+import Link from "next/link";
 import { Button } from "@/ui/button";
 import { Card, CardContent, CardHeader } from "@/ui/card";
 import { Bell, Search, ShoppingCart, User, CheckCircle2 } from "lucide-react";
@@ -13,13 +14,21 @@ export default function Header() {
     setIsShow(!isShow);
   }
   return (
-    <div className="h-auto flex flex-col items-center bg-background">
-      <header className="w-full bg-card p-4 flex justify-between items-center border-b">
+    <div className="h-auto flex flex-col items-center bg-background pt-20">
+      <header className="w-full bg-card p-4 flex justify-between items-center border-b fixed top-0 left-0 right-0 z-50">
         <div className="flex items-center space-x-4">
-          <span className="font-bold pr-5 border-r-2">METAHARBOR</span>
-          <span className="pl-5">Drops</span>
-          <span className="pl-5">Stats</span>
-          <span className="pl-5">Create</span>
+          <Link href="/">
+            <span className="font-bold pr-5 border-r-2 cursor-pointer">METAHARBOR</span>
+          </Link>
+          <Link href="/drops">
+            <span className=" cursor-pointer hover:text-primary hover:bg-slate-300 rounded-lg px-2 py-1">Drops</span>
+          </Link>
+          <Link href="/stats">
+            <span className=" cursor-pointer hover:text-primary  hover:bg-slate-300 rounded-lg px-2 py-1">Stats</span>
+          </Link>
+          <Link href="/create">
+            <span className=" cursor-pointer hover:text-primary hover:bg-slate-300 rounded-lg px-2 py-1">Create</span>
+          </Link>
         </div>
         <div className="flex justify-center flex-1 mx-4">
             <div className="flex items-center space-x-2 border rounded-lg px-3 py-2">
